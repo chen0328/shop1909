20501640f51a765b95d303bf9791a78e60ec3430
chen0328 <1833426479@qq.com>
2020-06-22 15:43:10 +0800

    git add .

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -13,8 +13,8 @@ class IndexController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function index(Request $request)
     {
-        
+        return view("admin/inc/index");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::any('admin/index', 'Admin\IndexController@index');
